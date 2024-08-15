8fae5c76135219f37f14b2161e555c3601b9afaf
Yehor Yehorychev <egor57899@gmail.com>
2024-08-14 18:47:56 -0700

    Dependency Injection functionality using setter methods added

--- src/test/java/com/yehorychev/spring/spring_selenium/User.java
+++ src/test/java/com/yehorychev/spring/spring_selenium/User.java
@@ -1,14 +1,26 @@
 package com.yehorychev.spring.spring_selenium;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class User {
+
     private Address address;
     private Salary salary;
 
-    public User(Address address, Salary salary) {
+    /*public User(Address address, Salary salary) {
         this.address = address;
+        this.salary = salary;
+    }*/
+
+    @Autowired
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Autowired
+    public void setSalary(Salary salary) {
         this.salary = salary;
     }
 
