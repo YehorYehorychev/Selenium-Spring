846e2266c0c635811eea005d5398b3a8e6de08d1
Yehor Yehorychev <egor57899@gmail.com>
2024-08-15 18:45:21 -0700

    Test class has been updated. First iteration of Value Injection was implemented

--- src/test/java/com/yehorychev/spring/spring_selenium/SpringSeleniumApplicationTests.java
+++ src/test/java/com/yehorychev/spring/spring_selenium/SpringSeleniumApplicationTests.java
@@ -2,6 +2,7 @@ package com.yehorychev.spring.spring_selenium;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -10,8 +11,11 @@ class SpringSeleniumApplicationTests {
 	@Autowired
 	private User user;
 
+	@Value("${PATH}")
+	private String path;
+
 	@Test
 	void contextLoads() {
-		user.printDetails();
+		System.out.println(this.path);
 	}
 }
