202ae44c705b7b24cae4fbcba4d3ce775b1860e5
Yehor Yehorychev <egor57899@gmail.com>
2024-08-15 18:53:51 -0700

    Test class has been updated. Third iteration of Value Injection was implemented

--- src/test/java/com/yehorychev/spring/spring_selenium/SpringSeleniumApplicationTests.java
+++ src/test/java/com/yehorychev/spring/spring_selenium/SpringSeleniumApplicationTests.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 class SpringSeleniumApplicationTests {
 
@@ -15,10 +17,11 @@ class SpringSeleniumApplicationTests {
 	private String path;
 
 	@Value("${spring.application.name}")
-	private String appName;
+	private List<String> appName;
 
 	@Test
 	void contextLoads() {
 		System.out.println(this.appName);
+		System.out.println(this.appName.size());
 	}
 }
