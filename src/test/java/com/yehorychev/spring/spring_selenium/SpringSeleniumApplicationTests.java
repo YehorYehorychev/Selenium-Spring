a35b977bfb08916806647958a3d2963360474fd8
Yehor Yehorychev <egor57899@gmail.com>
2024-08-15 19:21:33 -0700

    Spring Bean has been created for Faker library usage

--- src/test/java/com/yehorychev/spring/spring_selenium/SpringSeleniumApplicationTests.java
+++ src/test/java/com/yehorychev/spring/spring_selenium/SpringSeleniumApplicationTests.java
@@ -1,26 +1,21 @@
 package com.yehorychev.spring.spring_selenium;
 
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
 
 @SpringBootTest
 class SpringSeleniumApplicationTests {
 
-	@Autowired
-	private User user;
+    @Autowired
+    private User user;
 
-	@Value("${TEST_URL:https://www.google.com}")
-	private String path;
+    @Autowired
+    private Faker faker;
 
-	@Value("${spring.application.name}")
-	private List<String> appName;
-
-	@Test
-	void contextLoads() {
-		this.user.printDetails();
-	}
+    @Test
+    void contextLoads() {
+        System.out.println(faker.name().firstName());
+    }
 }
