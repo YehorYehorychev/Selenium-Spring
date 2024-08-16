6a7e8ae33067ca95f9f286fbb6c09a75363e2957
Yehor Yehorychev <egor57899@gmail.com>
2024-08-15 19:08:28 -0700

    Test class has been updated. Final iteration of Value Injection was implemented

--- src/test/java/com/yehorychev/spring/spring_selenium/Salary.java
+++ src/test/java/com/yehorychev/spring/spring_selenium/Salary.java
@@ -5,11 +5,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Salary {
-
-    @Value("${amount:500}")
     private int amount;
 
     public int getAmount() {
         return amount;
+    }
+
+    @Value("${amount:500}")
+    public void setAmount(int amount) {
+        this.amount = amount;
     }
 }
