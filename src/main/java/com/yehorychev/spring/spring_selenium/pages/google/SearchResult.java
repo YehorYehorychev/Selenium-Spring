a4022d42d71e540179c5e97b8139f90cdc373a80
Yehor Yehorychev <egor57899@gmail.com>
2024-08-19 17:59:01 -0700

    SearchResult class has been created

---
+++ src/main/java/com/yehorychev/spring/spring_selenium/pages/google/SearchResult.java
@@ -0,0 +1,22 @@
+package com.yehorychev.spring.spring_selenium.pages.google;
+
+import com.yehorychev.spring.spring_selenium.pages.Base;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class SearchResult extends Base {
+
+    @FindBy(css = "")
+    private List<WebElement> results;
+
+    public int getCount() {
+        return this.results.size();
+    }
+
+    @Override
+    public boolean isLoaded() {
+        return this.wait.until((d) -> !this.results.isEmpty());
+    }
+}
