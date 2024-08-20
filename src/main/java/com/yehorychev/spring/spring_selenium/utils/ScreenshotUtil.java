d40de5b3e30a110fe48ba1665f89bff52da524be
Yehor Yehorychev <egor57899@gmail.com>
2024-08-20 16:54:05 -0700

    ScreenshotUtil class methods created

--- src/main/java/com/yehorychev/spring/spring_selenium/utils/ScreenshotUtil.java
+++ src/main/java/com/yehorychev/spring/spring_selenium/utils/ScreenshotUtil.java
@@ -1,4 +1,24 @@
 package com.yehorychev.spring.spring_selenium.utils;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.nio.file.Path;
+
+@Component
 public class ScreenshotUtil {
+
+    @Autowired
+    private TakesScreenshot driver;
+
+    @Value("${screenshot.path}")
+    private Path path;
+
+    public void takeScreenshot() {
+        File file = this.driver.getScreenshotAs(OutputType.FILE);
+    }
 }
