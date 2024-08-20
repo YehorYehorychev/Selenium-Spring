775c1b0d95caa8799a2daf0dfc9e775ca9cc9093
Yehor Yehorychev <egor57899@gmail.com>
2024-08-19 18:09:46 -0700

    SearchResult class has been refactored and GoolePage class has been created

--- src/main/java/com/yehorychev/spring/spring_selenium/pages/google/SearchResult.java
+++ src/main/java/com/yehorychev/spring/spring_selenium/pages/google/SearchResult.java
@@ -3,9 +3,11 @@ package com.yehorychev.spring.spring_selenium.pages.google;
 import com.yehorychev.spring.spring_selenium.pages.Base;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class SearchResult extends Base {
 
     @FindBy(css = "div.g")
