1ccc3490c0d7b904c791b8b2608473df76b5997a
Yehor Yehorychev <egor57899@gmail.com>
2024-08-21 18:13:59 -0700

    ChromeDriver has been refactored to ConditionalOnMissingBean

--- src/main/java/com/yehorychev/spring/spring_selenium/config/WebDriverConfig.java
+++ src/main/java/com/yehorychev/spring/spring_selenium/config/WebDriverConfig.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,17 +23,17 @@ public class WebDriverConfig {
     private int timeout;
 
     @Bean
-    @ConditionalOnProperty(name = "browser", havingValue = "chrome")
-    public WebDriver chromeDriver() {
-        WebDriverManager.chromedriver().setup();
-        return new ChromeDriver();
-    }
-
-    @Bean
     @ConditionalOnProperty(name = "browser", havingValue = "firefox")
     public WebDriver firefoxDriver() {
         WebDriverManager.firefoxdriver().setup();
         return new FirefoxDriver();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public WebDriver chromeDriver() {
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
     }
 
     @Bean
