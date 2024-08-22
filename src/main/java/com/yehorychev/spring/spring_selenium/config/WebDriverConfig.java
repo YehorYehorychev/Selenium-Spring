27b47774fad54e2a9d8bd1fe23ca4a7386faec0f
Yehor Yehorychev <egor57899@gmail.com>
2024-08-21 17:05:47 -0700

    Firefox driver has been added

--- src/main/java/com/yehorychev/spring/spring_selenium/config/WebDriverConfig.java
+++ src/main/java/com/yehorychev/spring/spring_selenium/config/WebDriverConfig.java
@@ -3,6 +3,7 @@ package com.yehorychev.spring.spring_selenium.config;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +23,12 @@ public class WebDriverConfig {
     public WebDriver chromeDriver() {
         WebDriverManager.chromedriver().setup();
         return new ChromeDriver();
+    }
+
+    @Bean
+    public WebDriver firefoxDriver() {
+        WebDriverManager.firefoxdriver().setup();
+        return new FirefoxDriver();
     }
 
     @Bean
