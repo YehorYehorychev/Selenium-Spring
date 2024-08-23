224066876df42364fdef1971e896f4df1fc4240a
Yehor Yehorychev <egor57899@gmail.com>
2024-08-22 18:02:58 -0700

    RemoteWebDriverCofig class has been created and env file also been created

---
+++ src/main/java/com/yehorychev/spring/spring_selenium/config/RemoteWebDriverConfig.java
@@ -0,0 +1,11 @@
+package com.yehorychev.spring.spring_selenium.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+@Lazy
+@Configuration
+public class RemoteWebDriverConfig {
+
+
+}
