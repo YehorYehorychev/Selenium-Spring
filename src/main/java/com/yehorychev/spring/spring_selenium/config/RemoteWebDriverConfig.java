55319f0fac0a4b63af20998b3bf4e5aa647fdd89
Yehor Yehorychev <egor57899@gmail.com>
2024-08-23 17:06:14 -0700

    WebDriverWait bean created in separate class

--- src/main/java/com/yehorychev/spring/spring_selenium/config/RemoteWebDriverConfig.java
+++ src/main/java/com/yehorychev/spring/spring_selenium/config/RemoteWebDriverConfig.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -14,7 +13,6 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 
 import java.net.URL;
-import java.time.Duration;
 
 @Lazy
 @Configuration
@@ -23,9 +21,6 @@ public class RemoteWebDriverConfig {
 
     @Value("${selenium.grid.url}")
     private URL url;
-
-    @Value("${default.timeout}")
-    private int timeout;
 
     @Bean
     @ConditionalOnProperty(name = "browser", havingValue = "firefox")
@@ -39,11 +34,5 @@ public class RemoteWebDriverConfig {
     public WebDriver remoteChromeDriver() {
         ChromeOptions options = new ChromeOptions();
         return new RemoteWebDriver(this.url, options);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public WebDriverWait webDriverWait(WebDriver driver) {
-        return new WebDriverWait(driver, Duration.ofSeconds(this.timeout));
     }
 }
