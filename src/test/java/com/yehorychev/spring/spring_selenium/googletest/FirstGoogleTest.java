6f3d0c9b3b05f50e723053006c462d3fbd8305df
Yehor Yehorychev <egor57899@gmail.com>
2024-08-26 12:27:52 -0700

    SecondGoogleTest class has been created

---
+++ src/test/java/com/yehorychev/spring/spring_selenium/googletest/FirstGoogleTest.java
@@ -0,0 +1,30 @@
+package com.yehorychev.spring.spring_selenium.googletest;
+
+import com.yehorychev.spring.spring_selenium.SpringBaseTestNGTest;
+import com.yehorychev.spring.spring_selenium.pages.google.GooglePage;
+import com.yehorychev.spring.spring_selenium.utils.ScreenshotUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirstGoogleTest extends SpringBaseTestNGTest {
+
+    @Autowired
+    private GooglePage googlePage;
+
+    @Lazy
+    @Autowired
+    private ScreenshotUtil screenshotUtil;
+
+    @Test
+    public void googleTest() {
+        this.googlePage.goTo();
+        Assert.assertTrue(this.googlePage.isLoaded());
+
+        this.googlePage.getSearchComponent().search("Spring Boot");
+        Assert.assertTrue(this.googlePage.getSearchResult().isLoaded());
+        Assert.assertTrue(this.googlePage.getSearchResult().getCount() > 2);
+        this.screenshotUtil.takeScreenshot();
+    }
+}
