974cd196360fc3d8cda6e0ac85fc38f2c4b7e81d
Yehor Yehorychev <egor57899@gmail.com>
2024-08-26 14:31:36 -0700

    Bean scope functionality has been implemented

--- src/main/java/com/yehorychev/spring/spring_selenium/pages/google/GooglePage.java
+++ src/main/java/com/yehorychev/spring/spring_selenium/pages/google/GooglePage.java
@@ -3,25 +3,29 @@ package com.yehorychev.spring.spring_selenium.pages.google;
 import com.yehorychev.spring.spring_selenium.pages.Base;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+@Lazy
 @Component
+@Scope("prototype")
 public class GooglePage extends Base {
 
-   @Autowired
-   private SearchComponent searchComponent;
+    @Autowired
+    private SearchComponent searchComponent;
 
-   @Autowired
-   private SearchResult searchResult;
+    @Autowired
+    private SearchResult searchResult;
 
-   @Value("${application.url}")
-   private String url;
+    @Value("${application.url}")
+    private String url;
 
-   public void goTo() {
-       this.driver.get(url);
-   }
+    public void goTo() {
+        this.driver.get(url);
+    }
 
-   public SearchComponent getSearchComponent() {
+    public SearchComponent getSearchComponent() {
         return searchComponent;
     }
 
