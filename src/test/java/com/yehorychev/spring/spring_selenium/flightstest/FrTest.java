d997e578928b545f02b5757b5ba5016544fcf4af
Yehor Yehorychev <egor57899@gmail.com>
2024-08-30 15:16:57 -0700

    GoogleFlight Fr localization test has been implemented

---
+++ src/test/java/com/yehorychev/spring/spring_selenium/flightstest/FrTest.java
@@ -0,0 +1,8 @@
+package com.yehorychev.spring.spring_selenium.flightstest;
+
+import org.springframework.test.context.TestPropertySource;
+
+@TestPropertySource(properties = "language/fr.properties")
+public class FrTest extends FlightTest {
+
+}
