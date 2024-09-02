1ee50d6290090451d1b4201294bb189216944cfd
Yehor Yehorychev <egor57899@gmail.com>
2024-09-02 08:57:19 -0700

    switchByTitle method has been created

--- src/main/java/com/yehorychev/spring/spring_selenium/kelvin/service/WindowSwitchService.java
+++ src/main/java/com/yehorychev/spring/spring_selenium/kelvin/service/WindowSwitchService.java
@@ -1,5 +1,6 @@
 package com.yehorychev.spring.spring_selenium.kelvin.service;
 
+import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
@@ -10,5 +11,13 @@ public class WindowSwitchService {
     @Autowired
     private ApplicationContext ctx;
 
-
+    public void switchByTitle(final String title) {
+        WebDriver driver = this.ctx.getBean(WebDriver.class);
+        driver.getWindowHandles()
+                .stream()
+                .map(handle -> driver.switchTo().window(handle).getTitle())
+                .filter(t -> t.startsWith(title))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("No such window"));
+    }
 }
