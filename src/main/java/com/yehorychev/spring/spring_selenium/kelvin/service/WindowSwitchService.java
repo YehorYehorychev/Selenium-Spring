34fcee1b48a2efdebcd48e5581f979e318b74509
Yehor Yehorychev <egor57899@gmail.com>
2024-09-02 08:51:35 -0700

    WindowSwitchService class created

---
+++ src/main/java/com/yehorychev/spring/spring_selenium/kelvin/service/WindowSwitchService.java
@@ -0,0 +1,14 @@
+package com.yehorychev.spring.spring_selenium.kelvin.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WindowSwitchService {
+
+    @Autowired
+    private ApplicationContext ctx;
+
+
+}
