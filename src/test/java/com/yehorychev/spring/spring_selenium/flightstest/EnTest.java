b80da38eeea3cdb3ea878facca3dbc97169cd18c
Yehor Yehorychev <egor57899@gmail.com>
2024-09-02 09:46:50 -0700

    switchByIndex method has been created

--- src/test/java/com/yehorychev/spring/spring_selenium/flightstest/EnTest.java
+++ src/test/java/com/yehorychev/spring/spring_selenium/flightstest/EnTest.java
@@ -1,5 +1,6 @@
 package com.yehorychev.spring.spring_selenium.flightstest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.TestPropertySource;
 
 @TestPropertySource(properties = "language/en.properties")
