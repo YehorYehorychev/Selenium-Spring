3dd64db4cea6fcbe46d4e8871a004b7bbd7ce2aa
Yehor Yehorychev <egor57899@gmail.com>
2024-09-03 10:44:37 -0700

    Custom Window annotation has been created

---
+++ src/main/java/com/yehorychev/spring/spring_selenium/kelvin/aop/WindowAspect.java
@@ -0,0 +1,31 @@
+package com.yehorychev.spring.spring_selenium.kelvin.aop;
+
+import com.yehorychev.spring.spring_selenium.kelvin.annotations.Window;
+import com.yehorychev.spring.spring_selenium.kelvin.service.WindowSwitchService;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class WindowAspect {
+
+    @Autowired
+    private ApplicationContext ctx;
+
+    @Autowired
+    private WindowSwitchService windowSwitchService;
+
+    @Before("@target(window) && within(com.yehorychev.spring.spring_selenium..*)")
+    public void before(Window window) {
+        this.windowSwitchService.switchByTitle(window.value());
+    }
+
+    @After("@target(window) && within(com.yehorychev.spring.spring_selenium..*)")
+    public void after(Window window) {
+        this.windowSwitchService.switchByIndex(0);
+    }
+}
