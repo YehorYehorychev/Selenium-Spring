9f0ba6b418bfe74a875052ad010179ea43854202
Yehor Yehorychev <egor57899@gmail.com>
2024-09-03 10:04:40 -0700

    WindowSwitchTest has been created

---
+++ src/test/java/com/yehorychev/spring/spring_selenium/windowtest/WindowSwitchTest.java
@@ -0,0 +1,7 @@
+package com.yehorychev.spring.spring_selenium.windowtest;
+
+import com.yehorychev.spring.spring_selenium.SpringBaseTestNGTest;
+
+public class WindowSwitchTest  extends SpringBaseTestNGTest {
+
+}
