6eb2c63621b2d49a88129aaf7545cce971693fb4
Yehor Yehorychev <egor57899@gmail.com>
2024-09-03 10:08:09 -0700

    WindowSwitchTest has been implemented

--- src/test/java/com/yehorychev/spring/spring_selenium/windowtest/WindowSwitchTest.java
+++ src/test/java/com/yehorychev/spring/spring_selenium/windowtest/WindowSwitchTest.java
@@ -1,7 +1,40 @@
 package com.yehorychev.spring.spring_selenium.windowtest;
 
 import com.yehorychev.spring.spring_selenium.SpringBaseTestNGTest;
+import com.yehorychev.spring.spring_selenium.kelvin.service.WindowSwitchService;
+import com.yehorychev.spring.spring_selenium.pages.window.MainPage;
+import com.yehorychev.spring.spring_selenium.pages.window.PageA;
+import com.yehorychev.spring.spring_selenium.pages.window.PageB;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class WindowSwitchTest  extends SpringBaseTestNGTest {
 
+    @Autowired
+    private MainPage mainPage;
+
+    @Autowired
+    private PageA pageA;
+
+    @Autowired
+    private PageB pageB;
+
+    @Autowired
+    private WindowSwitchService windowSwitchService;
+
+    @BeforeClass
+    public void setup() {
+        this.mainPage.goTo();;
+        this.mainPage.isLoaded();
+        this.mainPage.launchAllWindows();
+    }
+
+    @Test
+    public void switchTest() {
+        this.windowSwitchService.switchByTitle("Page A");
+        this.pageA.addToArea("hi page A");
+        this.windowSwitchService.switchByIndex(2);
+        this.pageA.addToArea("hello page B");
+    }
 }
