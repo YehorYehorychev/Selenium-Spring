66860ed4061997904bd9b133b5a09818fdc49db5
Yehor Yehorychev <egor57899@gmail.com>
2024-09-04 17:55:35 -0700

    ScreenshotAspect has been implemented

--- src/main/java/com/yehorychev/spring/spring_selenium/kelvin/annotations/TakeScreenshot.java
+++ src/main/java/com/yehorychev/spring/spring_selenium/kelvin/annotations/TakeScreenshot.java
@@ -3,7 +3,7 @@ package com.yehorychev.spring.spring_selenium.kelvin.annotations;
 import java.lang.annotation.*;
 
 @Documented
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface TakeScreenshot {
 }
