0c9eb56eac6006a99acff1f8f085e0ad11923412
Yehor Yehorychev <egor57899@gmail.com>
2024-09-04 17:21:46 -0700

    Custom Window annotation has been refactored

--- src/test/java/com/yehorychev/spring/spring_selenium/windowtest/WindowSwitchTest.java
+++ src/test/java/com/yehorychev/spring/spring_selenium/windowtest/WindowSwitchTest.java
@@ -1,7 +1,6 @@
 package com.yehorychev.spring.spring_selenium.windowtest;
 
 import com.yehorychev.spring.spring_selenium.SpringBaseTestNGTest;
-import com.yehorychev.spring.spring_selenium.kelvin.service.WindowSwitchService;
 import com.yehorychev.spring.spring_selenium.pages.window.MainPage;
 import com.yehorychev.spring.spring_selenium.pages.window.PageA;
 import com.yehorychev.spring.spring_selenium.pages.window.PageB;
@@ -9,10 +8,11 @@ import com.yehorychev.spring.spring_selenium.pages.window.PageC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.TestPropertySource;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 @TestPropertySource(properties = "browser=chrome")
-public class WindowSwitchTest  extends SpringBaseTestNGTest {
+public class WindowSwitchTest extends SpringBaseTestNGTest {
 
     @Autowired
     private MainPage mainPage;
@@ -28,13 +28,26 @@ public class WindowSwitchTest  extends SpringBaseTestNGTest {
 
     @BeforeClass
     public void setup() {
-        this.mainPage.goTo();;
+        this.mainPage.goTo();
         this.mainPage.isLoaded();
         this.mainPage.launchAllWindows();
     }
 
-    @Test
-    public void switchTest() {
+    @Test(dataProvider = "getData")
+    public void switchTest(int index) {
+        this.pageA.addToArea(index + "\n");
+        this.pageB.addToArea((index * 2) + "\n");
+        this.pageC.addToArea((index * 3) + "\n");
+    }
 
+    @DataProvider
+    public Object[] getData() {
+        return new Object[] {
+                3,
+                4,
+                1,
+                5,
+                2
+        };
     }
 }
