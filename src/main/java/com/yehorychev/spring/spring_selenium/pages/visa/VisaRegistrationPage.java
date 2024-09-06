04a3004b5d557becd6bec926f1f913cae882b9e1
Yehor Yehorychev <egor57899@gmail.com>
2024-09-06 09:49:59 -0700

    VisaRegistrationPage has been updated

--- src/main/java/com/yehorychev/spring/spring_selenium/pages/visa/VisaRegistrationPage.java
+++ src/main/java/com/yehorychev/spring/spring_selenium/pages/visa/VisaRegistrationPage.java
@@ -1,11 +1,52 @@
 package com.yehorychev.spring.spring_selenium.pages.visa;
 
+import com.yehorychev.spring.spring_selenium.kelvin.annotations.Page;
 import com.yehorychev.spring.spring_selenium.pages.Base;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+@Page
 public class VisaRegistrationPage extends Base {
 
+    @FindBy(id ="first_4")
+    private WebElement firstName;
 
+    @FindBy(id ="last_4")
+    private WebElement lastName;
 
+    @FindBy(id = "input_46")
+    private WebElement fromCountry;
+
+    @FindBy(id = "input_47")
+    private WebElement toCountry;
+
+    @FindBy(id = "input_24_month")
+    private WebElement month;
+
+    @FindBy(id = "input_24_day")
+    private WebElement day;
+
+    @FindBy(id = "input_24_year")
+    private WebElement year;
+
+    @FindBy(id = "input_6")
+    private WebElement email;
+
+    @FindBy(id = "input_27_phone")
+    private WebElement phone;
+
+    @FindBy(id = "input_45")
+    private WebElement comments;
+
+    @FindBy(id = "submitBtn")
+    private WebElement submit;
+
+    @FindBy(id = "requestnumber")
+    private WebElement requestNumber;
+
+    public void goTo(){
+        this.driver.get("https://vins-udemy.s3.amazonaws.com/sb/visa/udemy-visa.html");
+    }
 
     @Override
     public boolean isLoaded() {
