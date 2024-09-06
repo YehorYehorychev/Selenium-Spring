6f9667b339d85d1f33b4903d93697f9670df418c
Yehor Yehorychev <egor57899@gmail.com>
2024-09-06 09:48:16 -0700

    VisaRegistrationPage has been created

---
+++ src/main/java/com/yehorychev/spring/spring_selenium/pages/visa/VisaRegistrationPage.java
@@ -0,0 +1,14 @@
+package com.yehorychev.spring.spring_selenium.pages.visa;
+
+import com.yehorychev.spring.spring_selenium.pages.Base;
+
+public class VisaRegistrationPage extends Base {
+
+
+
+
+    @Override
+    public boolean isLoaded() {
+        return false;
+    }
+}
