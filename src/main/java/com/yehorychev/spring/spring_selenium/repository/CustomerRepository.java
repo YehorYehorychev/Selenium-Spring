50bfb1adf074e538b87e4d0e3064661064bcd7db
Yehor Yehorychev <egor57899@gmail.com>
2024-09-09 18:21:55 -0700

    UserVisaTest class has been refactored

--- src/main/java/com/yehorychev/spring/spring_selenium/repository/CustomerRepository.java
+++ src/main/java/com/yehorychev/spring/spring_selenium/repository/CustomerRepository.java
@@ -4,9 +4,11 @@ import com.yehorychev.spring.spring_selenium.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
 import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     List<Customer> findByFirstNameStartingWith(String startsWith);
+    List<Customer> findByDobBetween(Date from, Date to);
 }
