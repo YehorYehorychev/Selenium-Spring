634bd78b93e772a7ba99552f9110ccf6769b0c84
Yehor Yehorychev <egor57899@gmail.com>
2024-09-12 16:36:52 -0700

    Cucumber Hooks afterScenario method has been implemented

--- src/test/java/com/yehorychev/spring/spring_selenium/CucumberRunner.java
+++ src/test/java/com/yehorychev/spring/spring_selenium/CucumberRunner.java
@@ -2,6 +2,7 @@ package com.yehorychev.spring.spring_selenium;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "classpath:features",
@@ -13,4 +14,10 @@ import io.cucumber.testng.CucumberOptions;
         monochrome = true
 )
 public class CucumberRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
--- src/test/java/com/yehorychev/spring/spring_selenium/stepdefinitions/CucumberHooks.java
+++ src/test/java/com/yehorychev/spring/spring_selenium/stepdefinitions/CucumberHooks.java
@@ -2,18 +2,30 @@ package com.yehorychev.spring.spring_selenium.stepdefinitions;
 
 import com.yehorychev.spring.spring_selenium.kelvin.annotations.LazyAutowired;
 import com.yehorychev.spring.spring_selenium.kelvin.service.ScreenshotService;
+import io.cucumber.java.After;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Scenario;
+import org.openqa.selenium.WebDriver;
 
 public class CucumberHooks {
 
     @LazyAutowired
     private ScreenshotService screenshotService;
 
+    @LazyAutowired
+    private WebDriver driver;
+
     @AfterStep
     public void afterStep(Scenario scenario) {
         if (scenario.isFailed()) {
             scenario.attach(this.screenshotService.getScreenshot(), "image/png", scenario.getName());
+        }
+    }
+
+    @After
+    public void afterScenario() {
+        if (driver != null) {
+            driver.quit();
         }
     }
 }
